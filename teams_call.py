ddf9a2d76e6141b80b9e5b04230f9e6fc4e51a34
Mindaugas Milius <milius@eventit.ag>
2022-03-30 19:45:26 +0200

    better python filename

--- teams_call.py
+++ teams_call.py
@@ -1,31 +1,30 @@
 import os
+import sys
 from pathlib import Path
-import re
 
 def getLogFile():
-    uname = os.uname()
-    home = str(Path.home())
+    if sys.argv[1]:
+        return sys.argv[1]
 
-    match uname.sysname:
-        case 'Linux':
-            return home + '/.config/Microsoft/Microsoft Teams/logs.txt'
-        case 'Darwin':
-            return home + '/Library/Application Support/Microsoft/Teams/logs.txt'
-        case _:
-            raise Exception('OS not supported!')
+    if os.uname().sysname == 'Linux':
+        return str(Path.home()) + '/.config/Microsoft/Microsoft Teams/logs.txt'
+    if os.uname().sysname == 'Darwin':
+        return str(Path.home()) + '/Library/Application Support/Microsoft/Teams/logs.txt'
+
+    raise Exception('logs file path not given as argument or OS not supported!')
 
 def isInCall():
     logFile = getLogFile()
     if not os.path.isfile(logFile):
-        raise Exception('Log file not found: ' + logFile)
+        print(f'Log file not found: {logFile}')
+        return False
 
     output = os.popen('tac "' + logFile +  '" | grep -oh "eventData: s::;m::1;a::[0-9]" | head -n1').read().split('\n')
 
-    if output[0][-1] in ['0', '1']:
-        #print("In Call")
-        return True
-    else:
-        #print("Not In Call")
+    try:
+        return output[0][-1] in ['0', '1']
+    except Exception as err:
+        print("Error:", err)
         return False
 
 if __name__ == '__main__':
